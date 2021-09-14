1c790cdf253d2bb51420074bf592ff91f03ca7f8
Ihar Shyrynkou <cupofjoyness@gmail.com>
2021-09-14 11:49:35 +0300

    Added layered structure

---
+++ WebForecast/PresentationLayer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PresentationLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var city = Console.ReadLine();
+            Console.WriteLine("Weather forecast for now: ");
+        }
+    }
+}
